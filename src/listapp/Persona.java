c3103e6cd803b9b6d90ff9ae45d551ca95bdc5d4
Marlon C.S <marlsas0306@gmail.com>
2018-04-17 17:15:27 -0500

    add getter and setters

--- src/listapp/Persona.java
+++ src/listapp/Persona.java
@@ -24,7 +24,47 @@ public class Persona {
         this.sexo = sexo;
     }
 
-  
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    
     
     @Override
     public boolean equals(Object obj) {
